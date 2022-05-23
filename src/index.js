9453af7d3ecd1123cb1affc7036a2f17d2bd6825
Harsha vardhan <bavardha@gmail.com>
2022-05-23 23:14:34 +0530

    Added persistent storage

--- src/index.js
+++ src/index.js
@@ -5,17 +5,22 @@ import 'antd/dist/antd.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {store} from './store';
+import store from './store';
 import {Provider} from 'react-redux';
 
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+let persistor = persistStore(store)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,37 @@ import { configureStore } from '@reduxjs/toolkit'
 import tasksReducer from '../features/tasks/taskSlice'
 import calendarReducer from '../features/calendar/calendarSlice'
 import searchReducer from '../features/search/searchSlice'
-export const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-    calendar: calendarReducer,
-    search: searchReducer,
-  },
+
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import thunk from 'redux-thunk'
+
+const reducers = combineReducers({
+  tasks: tasksReducer,
+  calendar: calendarReducer,
+  search: searchReducer,
 })
+
+// export const store = configureStore({
+//   reducer: {
+//     tasks: tasksReducer,
+//     calendar: calendarReducer,
+//     search: searchReducer,
+//   },
+// })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+})
+
+export default store
